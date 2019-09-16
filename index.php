5ebd6a99c667ebd9df921f5bdf24610b6b329bba
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2019-09-16 14:01:22 +0200

    removed mongodb testÃ

--- index.php
+++ index.php
@@ -9,63 +9,6 @@ require_once('view/LayoutView.php');
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-// MongoDB setup
-$uri = 'mongodb://heroku_crxh706j:47oq86g7p9fa16diq95k3tjpu7@ds247410.mlab.com:47410/heroku_crxh706j';
-$client = new MongoDB\Client($uri);
-
-// Test snippet
-require 'vendor/autoload.php'; // include Composer's autoloader
-// Create seed data
-$seedData = array(
-    array(
-        'decade' => '1970s', 
-        'artist' => 'Debby Boone',
-        'song' => 'You Light Up My Life', 
-        'weeksAtOne' => 10
-    ),
-    array(
-        'decade' => '1980s', 
-        'artist' => 'Olivia Newton-John',
-        'song' => 'Physical', 
-        'weeksAtOne' => 10
-    ),
-    array(
-        'decade' => '1990s', 
-        'artist' => 'Mariah Carey',
-        'song' => 'One Sweet Day', 
-        'weeksAtOne' => 16
-    ),
-);
-$songs = $client->db->songs;
-// To insert a dict, use the insert method.
-$songs->insertMany($seedData);
-/*
- * Then we need to give Boyz II Men credit for their contribution to
- * the hit "One Sweet Day".
-*/
-$songs->updateOne(
-    array('artist' => 'Mariah Carey'), 
-    array('$set' => array('artist' => 'Mariah Carey ft. Boyz II Men'))
-);
-/*
- * Finally we run a query which returns all the hits that spent 10 
- * or more weeks at number 1. 
-*/
-$query = array('weeksAtOne' => array('$gte' => 10));
-$options = array(
-    "sort" => array('decade' => 1),
-);
-$cursor = $songs->find($query,$options);
-foreach($cursor as $doc) {
-    echo 'In the ' .$doc['decade'];
-    echo ', ' .$doc['song']; 
-    echo ' by ' .$doc['artist'];
-    echo ' topped the charts for ' .$doc['weeksAtOne']; 
-    echo ' straight weeks.', "\n";
-}
-// Since this is an example, we'll clean up after ourselves.
-//$songs->drop();
-
 
 //CREATE OBJECTS OF THE VIEWS
 $v = new LoginView();
