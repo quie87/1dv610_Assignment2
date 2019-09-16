e9f0ddf6d1229f987f64133cbc5ac3773c0838fa
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2019-09-16 15:58:23 +0200

    connecting to JAWSDB

--- index.php
+++ index.php
@@ -15,6 +15,24 @@ $v = new LoginView();
 $dtv = new DateTimeView();
 $lv = new LayoutView();
 
+// Test JAWSDB
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+var_dump('Connection was successfully established!');
+
 
 $lv->render(false, $v, $dtv);
 
