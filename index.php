2afbc45fcc62b6efe2d18a200c3d883c61b2cf34
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2019-09-20 18:04:00 +0200

    testing

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  require_once('view/LayoutView.php');
  require_once('model/DateTimeModel.php');
  require_once('model/LoginModel.php');
- require_once('controller/AuthenticationController.php');
+//  require_once('controller/AuthenticationController.php');
 //  require_once('AuthenticationApplication.php');
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
