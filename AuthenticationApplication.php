7455f1f396bb1b153adf231dd1e8f91c4e48bebc
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2019-09-24 12:59:07 +0200

    adding some skelleton code reguarding cookie and session inside Authapp

--- AuthenticationApplication.php
+++ AuthenticationApplication.php
@@ -1,22 +1,31 @@
 <?php
+session_start();
 
 //INCLUDE THE FILES NEEDED...
 require_once('view/LoginView.php');
 require_once('view/DateTimeView.php');
 require_once('view/LayoutView.php');
+
 require_once('model/DateTimeModel.php');
 require_once('model/AuthenticationModel.php');
 require_once('model/UserModel.php');
-require_once('controller/LoginController.php');
 require_once('model/AuthenticationModel.php');
+
+require_once('controller/LoginController.php');
 
 class AuthenticationApplication {
     private $loginView;
     private $dateTimeView;
     private $layoutView;
-    private $loginController;
-    private $isLoggedIn;
+
     private $authenticationModel;
+    
+    private $loginController;
+    private $cookie;
+    private $storage;
+    
+    private $isLoggedIn;
+    
 
     public function __construct()
     {
@@ -34,24 +43,42 @@ class AuthenticationApplication {
 
     
 	private function changeState() {
-        $this->isLoggedIn = $this->authenticationModel->getIsUserLoggedIn();
-        // TODO: Implement function to see if there is a session and a function to see if there is a cookie saved. Aka, if there is a user stored
-        // If no session and no cookie, check if user wants to register or tries to login
-        // if (!$this->isLoggedIn && $this->loginView->userWantToLogIn) {
-        // } else {
-        //     $this->registerController->register();
-        // }
-        if (!$this->isLoggedIn) {
+        if (!$this->isLoggedIn && !$this->cookie->getCookie()) {
             $wantToLoggIn = $this->loginView->userWantToLogIn();
             
             if($wantToLoggIn) {
                 $this->isLoggedIn = $this->loginController->login();
             }
-            //else if $this->registerController->register();
-            else if ($this->isLoggedIn) {
-                $this->loginController->logout();
-            }
+            //else if $this->registerController->WantsToRegister();
+            // $this->registerController->register();
+
+        } else if (!$this->isLoggedIn && $this->cooki->getCookie) {
+            $this->isLoggedIn = true;
+        } else if ($this->isLoggedIn) {
+            // First make sure that the user have clicked the logoutbutton
+            $this->loginController->logout();
+        }else {
+            throw new Exception('Something went wrong');
         }
+
+        // TODO: Implement function to see if there is a session and a function to see if there is a cookie saved. Aka, if there is a user stored
+        // If no session and no cookie, check if user wants to register or tries to login
+        // if (!$this->isLoggedIn && $this->loginView->userWantToLogIn) {
+            // } else {
+                //     $this->registerController->register();
+                // }
+        // $this->isLoggedIn = $this->authenticationModel->getIsUserLoggedIn();
+        // if (!$this->isLoggedIn) {
+        //     $wantToLoggIn = $this->loginView->userWantToLogIn();
+            
+        //     if($wantToLoggIn) {
+        //         $this->isLoggedIn = $this->loginController->login();
+        //     }
+        //     //else if $this->registerController->register();
+        //     else if ($this->isLoggedIn) {
+        //         $this->loginController->logout();
+        //     }
+        // }
     }
     
 	private function generateOutput() {
