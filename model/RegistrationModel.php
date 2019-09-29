2406aee29f057d075c27ec415916e1e47697f7c2
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2019-09-29 18:47:58 +0200

    refactoring

--- model/RegistrationModel.php
+++ model/RegistrationModel.php
@@ -12,19 +12,26 @@ class RegistrationModel {
 
     public function __construct(string $userName, string $password, string $repeatPassword)
     {
-        $this->checkIfContainsInvalidCharacters($userName);
-        
+        $this->validateNewUserCredentials($userName, $password, $repeatPassword);
+
         $this->userName = $this->applyFilter($userName);
         $this->password = $this->applyFilter($password);
         $this->repeatPassword = $this->applyFilter($repeatPassword);
 
-        if (strlen($this->userName) < $this->userNameLength) {
+    }
+
+    private function validateNewUserCredentials($userName, $password, $repeatPassword) {
+        if ($this->checkIfContainsInvalidCharacters($userName)) {
+            throw new UserHasInvalidCharacters('Username contains invalid characters.');
+        }
+
+        if (strlen($userName) < $this->userNameLength) {
             throw new UserNameToShortException("Username has too few characters, at least 3 characters.");
         }
-        if (strlen($this->password) < $this->passwordLength) {
+        if (strlen($password) < $this->passwordLength) {
             throw new PasswordIsToShortException("Password has too few characters, at least 6 characters.");
         }
-        if (!$this->passwordMatch()) {
+        if (!$this->passwordMatch($password, $repeatPassword)) {
             throw new PasswordDidNotMatchException("Passwords do not match.");
         }
     }
@@ -45,11 +52,8 @@ class RegistrationModel {
         return $userName != strip_tags($userName) ? true : false; 
     }
 
-    private function passwordMatch() : bool {
-        $pwd = $this->getUserPassword();
-        $pwd2 = $this->getRepeatPassword();
-
-        return ($pwd == $pwd2) ? true: false;
+    private function passwordMatch($password, $repeatPassword) : bool {
+        return $password == $repeatPassword ? true : false;
     }
 
     public static function applyFilter(string $rawInput) : string {
