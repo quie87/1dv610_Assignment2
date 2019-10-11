4676e4f1925c4fd4945c00d429ef872d6bb954c7
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2019-10-11 17:54:52 +0200

    try to fix the 500 error I got after moving files

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once('AuthenticationApplication/AuthenticationApplication.php');
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
+ini_set('display_startup_errors', 'on');
 
 // Initiat the application
 $app = new AuthenticationApplication();
