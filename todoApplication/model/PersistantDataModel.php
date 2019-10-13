45927eb30bda174427259812305e1e65e009e760
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2019-10-13 15:56:51 +0200

    save todo to json file

--- todoApplication/model/PersistantDataModel.php
+++ todoApplication/model/PersistantDataModel.php
@@ -5,13 +5,48 @@ namespace TodoModel;
 class PersistantDataModel
 {
     public function saveTodo($newTodo) {
-        $json = file_get_contents(__DIR__ . '/todos.json');
+        $file = (__DIR__ . '/todos.json');
+        $todos = json_decode(file_get_contents($file), true);
 
-        $json .= $newTodo->getTodo() . ', '; 
-        $file = fopen(__DIR__ . '/todos.json','w');
-        fwrite($file, $json);
-        fclose($file);
+        $newTodo = $newTodo->getTodo();
+
+        if ($todos) {
+            array_push($todos, $newTodo);
+        } else {
+            $todos = array ($newTodo);
+        }
+
+        $json = json_encode($todos);
+        file_put_contents($file, $json);
+
+        
+        // $json = json_decode(file_get_contents(__DIR__ . '/todos.json'));
+        // $newJsonString = $json . " " . $newTodo->getTodo();
+
+        // $file = fopen(__DIR__ . '/todos.json','w');
+        // fwrite($file, json_encode($newJsonString, JSON_PRETTY_PRINT));
+        // fclose($file);
 
 
+        // $array = array('todo' => $todos, $newTodo);
+        // $fp = fopen(__DIR__ . '/todos.json','w');
+        // fwrite($fp, json_encode($array, JSON_PRETTY_PRINT));   //here it will print the array pretty
+        // fclose($fp);
     }
+
+    // public function deleteTodo($todo)
+    // {
+    //     $jsonString = file_get_contents('jsonFile.json');
+    //     $data = json_decode($jsonString, true);
+        
+    //     $data[0]['activity_name'] = "TENNIS";
+    //     // or if you want to change all entries with activity_code "1"
+    //     foreach ($data as $key => $entry) {
+    //         if ($entry['activity_code'] == '1') {
+    //             $data[$key]['activity_name'] = "TENNIS";
+    //         }
+    //     }
+    //     $newJsonString = json_encode($data);
+    //     file_put_contents('jsonFile.json', $newJsonString);
+    // }
 }
